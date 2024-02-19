1630e94841d51c333bf22f9f1d94a600e3b4bc28
HP-W10 <HP-W10@DESKTOP-SK7KKJE>
2024-02-19 10:12:39 +0200

    done

--- Statment_Brancch_coverage/src/test/java/Statment_Brancch_coverage/Statment_Brancch_coverage/AppTest.java
+++ Statment_Brancch_coverage/src/test/java/Statment_Brancch_coverage/Statment_Brancch_coverage/AppTest.java
@@ -1,7 +1,5 @@
 package Statment_Brancch_coverage.Statment_Brancch_coverage;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -51,4 +49,59 @@ public class AppTest
         assertEquals(expected, result,0.001);
         
     }
+    @Test
+    public void testWeakMethod1A() {
+    	
+        assertEquals(15, WeakClass.weakMethod1(12, 0)); // תקלת חלוקה באפס
     }
+    @Test
+    public void testweakmethod1B() {
+		assertEquals(6, WeakClass.weakMethod1(5, 5));
+		assertEquals(25, WeakClass.weakMethod1(11, 11));
+
+		
+		
+	}
+    @Test
+    public void testweakmethod2A() {
+        assertEquals(4, WeakClass.weakMethod2(4, 0)); // תקלת חלוקה באפס
+
+    	
+
+    }
+    @Test
+    public void testweakmethod2B() {
+        assertEquals(17, WeakClass.weakMethod2(11, 1)); 
+        assertEquals(23, WeakClass.weakMethod2(7, 6)); 
+    }
+    @Test
+    public void testweakmethod3A() {
+    	assertEquals(5, WeakClass.weakMethod3(6, 3)); 
+        assertEquals(10, WeakClass.weakMethod3(4, 2)); 
+
+    	
+
+    }
+    @Test
+    public void testweakmethod3B() {
+        assertEquals(14, WeakClass.weakMethod3(14, 0)); 
+        assertEquals(10, WeakClass.weakMethod3(4, 2)); 
+
+    	
+    }
+    @Test
+    public void testweakmethod4A() {
+    	assertEquals(5, WeakClass.weakMethod4(10, 2)); 
+        assertEquals(10, WeakClass.weakMethod4(4, 2)); 
+
+    	
+
+    }
+    @Test
+    public void testweakmethod4B() {
+        assertEquals(6, WeakClass.weakMethod4(6, 0)); 
+
+    	
+}
+}
+    
